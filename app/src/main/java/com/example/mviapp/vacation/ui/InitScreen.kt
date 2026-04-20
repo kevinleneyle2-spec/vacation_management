c473f4d5294ec6a7effd1d86b5cc7c6e3b265003
kevin leneyle <kevin.leneyle2@gmail.com>
2026-04-20 19:13:27 +0200

    [TASK-13] Add activities and ideas dialog to lighten the screen

--- app/src/main/java/com/example/mviapp/vacation/ui/InitScreen.kt
+++ app/src/main/java/com/example/mviapp/vacation/ui/InitScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -114,7 +115,17 @@ fun InitScreen(
                 }
             }
         ) {
-            DatePicker(state = datePickerState)
+            DatePicker(
+                state = datePickerState,
+                colors = DatePickerDefaults.colors(
+                    selectedDayContainerColor = colorResource(R.color.orange),
+                    selectedDayContentColor = Color.White,
+                    selectedYearContainerColor = colorResource(R.color.orange),
+                    selectedYearContentColor = Color.White,
+                    todayContentColor = colorResource(R.color.orange),
+                    todayDateBorderColor = colorResource(R.color.orange)
+                )
+            )
         }
     }
 
